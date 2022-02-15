8417606b022a2dd801132d3d6080b5790d46528d
hasanmansur <smhasanmansur@gmail.com>
2022-02-14 22:13:17 -0500

    added # upto PCP pattern

--- text_analysis/pattern_matching/matching.py
+++ text_analysis/pattern_matching/matching.py
@@ -16,12 +16,22 @@ def on_match_callback(matcher, doc, i, matches):
     span = doc[start:end]  # The matched span
     print(string_id, " : " , span.text)
 
-# Add match ID, patterns, callback
+# Define pattern match ID with corresponding patterns and callback
 matcher.add("patterns_activity_message", patterns.patterns_activity_message, on_match=on_match_callback)
 matcher.add("patterns_high_demand_message", patterns.patterns_high_demand_message, on_match=on_match_callback)
 matcher.add("patterns_low_stock_message", patterns.patterns_low_stock_message, on_match=on_match_callback)
 matcher.add("patterns_limited_time_message", patterns.patterns_limited_time_message, on_match=on_match_callback)
 matcher.add("patterns_countdown_timer", patterns.patterns_countdown_timer, on_match=on_match_callback)
+matcher.add("patterns_false_hierarchy", patterns.patterns_false_hierarchy, on_match=on_match_callback)
+matcher.add("patterns_attention_distraction", patterns.patterns_attention_distraction, on_match=on_match_callback)
+matcher.add("patterns_default_choice", patterns.patterns_default_choice, on_match=on_match_callback)
+matcher.add("patterns_friend_spam", patterns.patterns_friend_spam, on_match=on_match_callback)
+matcher.add("patterns_forced_enrollment", patterns.patterns_forced_enrollment, on_match=on_match_callback)
+matcher.add("patterns_disguised_ads", patterns.patterns_disguised_ads, on_match=on_match_callback)
+matcher.add("patterns_social_pyramid", patterns.patterns_social_pyramid, on_match=on_match_callback)
+matcher.add("patterns_privacy_zuckering", patterns.patterns_privacy_zuckering, on_match=on_match_callback)
+matcher.add("patterns_intermediate_currency", patterns.patterns_intermediate_currency, on_match=on_match_callback)
+matcher.add("patterns_price_comparison_prevention", patterns.patterns_price_comparison_prevention, on_match=on_match_callback)
 
 # Test: OCR JSON file
 def match_patterns(file):
@@ -32,8 +42,12 @@ def match_patterns(file):
         doc = nlp(text_segment["content"])
         matches = matcher(doc)
 
-files = [file for file in glob.glob("../UIED/data/output/ocr/" + "*.json")]
+files = [file for file in glob.glob("../../UIED/data/output/ocr/" + "*.json")]
 files.sort()
 for file in files:
     print('----------- processing: ', file, '-------------')
     match_patterns(file)
+
+# # Test: single sentence
+# doc = nlp("decline the bonus Stone Magazine subscription Rolling to")
+# matches = matcher(doc)
--- text_analysis/pattern_matching/patterns.py
+++ text_analysis/pattern_matching/patterns.py
@@ -179,3 +179,197 @@ pattern4_countdown_timer = [
 # tba: time remaining
 
 patterns_countdown_timer = [pattern1_countdown_timer, pattern2_countdown_timer, pattern3_countdown_timer, pattern4_countdown_timer]
+
+# false hierarchy
+# ==============================================================================
+# - keywords (bigger size): start, turn, add, yes,next, sign in/up, ok, continue, unlock, subscribe, confirm,setup etc.
+# - keywords (smaller size):  no, not now, later, cancel, skip, exit etc.
+
+pattern1_false_hierarchy = [
+    {"LOWER": {"IN": ["start", "turn", "add", "yes", "next", "sign", "ok", "continue", "unlock", "subscribe", "confirm", "setup"]}},
+    {"LOWER": {"IN": ["on", "in", "up", "off"]}, "OP": "*"}
+]
+
+pattern2_false_hierarchy = [
+    {"LOWER": {"IN": ["no", "not", "later", "cancel", "skip", "exit"]}},
+    {"LOWER": {"IN": ["now", "thanks", "up", "later", "anytime"]}, "OP": "*"}
+]
+
+patterns_false_hierarchy = [pattern1_false_hierarchy, pattern2_false_hierarchy]
+
+
+# attention distraction
+# ==============================================================================
+# - keywords (bigger size): start, turn, add, yes,next, sign in/up, ok, continue, unlock, subscribe, confirm,setup etc.
+# - keywords (smaller size):  no, not now, later, cancel, skip, exit etc.
+# - I decline/don't want/opt out/refuse...to...
+
+pattern1_attention_distraction = [
+    {"LOWER": {"IN": ["start", "turn", "add", "yes", "next", "sign", "ok", "continue", "unlock", "subscribe", "confirm", "setup"]}},
+    {"LOWER": {"IN": ["on", "in", "up", "off"]}, "OP": "*"}
+]
+
+pattern2_attention_distraction = [
+    {"LOWER": {"IN": ["no", "not", "later", "cancel", "skip", "exit"]}},
+    {"LOWER": {"IN": ["now", "thanks", "up", "later", "anytime"]}, "OP": "*"}
+]
+
+pattern3_attention_distraction = [
+    {"LOWER": {"IN": ["decline", "refuse", "don't", "opt"]}},
+    {"LOWER": {"IN": ["want", "out"]}, "OP": "?"}
+]
+
+patterns_attention_distraction = [pattern1_attention_distraction, pattern2_attention_distraction, pattern3_attention_distraction]
+
+# default choice
+# ==============================================================================
+# - keywords (bigger size): start, turn, add, yes,next, sign in/up, ok, continue, unlock, subscribe, confirm,setup etc.
+# - keywords (smaller size):  no, not now, later, cancel, skip, exit etc.
+
+#  notify/notification
+
+# I agree/consent to ...', 'I give permission to ...'
+# by giving us permission/consent ...'
+# by clicking/pressing .....agree/consent to ....'
+# by subscribing ....agree/consent to ....
+
+pattern1_default_choice = [
+    {"LOWER": {"IN": ["start", "turn", "add", "yes", "next", "sign", "ok", "continue", "unlock", "subscribe", "confirm", "setup"]}},
+    {"LOWER": {"IN": ["on", "in", "up", "off"]}, "OP": "*"}
+]
+
+pattern2_default_choice = [
+    {"LOWER": {"IN": ["no", "not", "later", "cancel", "skip", "exit"]}},
+    {"LOWER": {"IN": ["now", "thanks", "up", "later", "anytime"]}, "OP": "*"}
+]
+
+pattern3_default_choice = [
+    {"LOWER": {"IN": ["notify", "notification"]}}
+]
+
+pattern4_default_choice = [
+    {"LOWER": {"IN": ["agree", "consent"]}},
+    {"LOWER": {"IN": ["permission", "consent"]}, "OP": "?"}
+]
+
+pattern5_default_choice = [
+    {"LEMMA": {"IN": ["give"]}},
+    {"IS_ALPHA": True, "OP": "*"},
+    {"LEMMA": {"IN": ["permission", "consent"]}}
+]
+
+pattern6_default_choice = [
+    {"LEMMA": {"IN": ["click", "press", "subscribe", "use"]}},
+    {"IS_ALPHA": True, "OP": "*"},
+    {"LEMMA": {"IN": ["agree", "consent", "give"]}},
+    {"IS_ALPHA": True, "OP": "*"},
+    {"LEMMA": {"IN": ["permission", "consent"]}, "OP": "?"}
+]
+
+patterns_default_choice = [
+    pattern1_default_choice, pattern2_default_choice, pattern3_default_choice,
+    pattern4_default_choice, pattern5_default_choice, pattern6_default_choice
+]
+
+# friend spam
+# ==============================================================================
+# upload/sync/share....contacts....',
+# invite/add friends...
+
+pattern1_friend_spam = [
+    {"LOWER": {"IN": ["upload", "sync", "share", "add", "invite"]}},
+    {"LOWER": {"IN": ["friends", "contacts"]}}
+]
+
+patterns_friend_spam = [pattern1_friend_spam]
+
+# forced enrollment
+# ==============================================================================
+# want/like to join/subscribe to ... and ..... terms and conditions ... [usually appears inline]
+
+pattern1_forced_enrollment = [
+    {"LOWER": {"IN": ["want", "like", "would"]}},
+    {"IS_ALPHA": True, "OP": "*"},
+    {"LOWER": {"IN": ["join", "subscribe"]}}
+]
+
+pattern2_forced_enrollment = [
+    {"LOWER": {"IN": ["terms"]}},
+    {"IS_ALPHA": True, "OP": "*"},
+    {"LOWER": {"IN": ["conditions"]}}
+]
+
+patterns_forced_enrollment = [pattern1_forced_enrollment, pattern2_forced_enrollment]
+
+# disguised ads
+# ==============================================================================
+# AdChoices, Install now, Download now
+
+pattern1_disguised_ads = [
+    {"LOWER": {"IN": ["ad", "adchoices"]}},
+]
+
+pattern2_disguised_ads = [
+    {"LOWER": {"IN": ["install", "download"]}},
+    {"LOWER": {"IN": ["now"]}},
+]
+
+patterns_disguised_ads = [pattern1_disguised_ads, pattern2_disguised_ads]
+
+# # social pyramid
+# # ==============================================================================
+# ask/invite/refer/signup
+# contacts/friends...to...'
+# 'unlock/earn/get/collect .... credits/rewards/points/tokens ....for....signup....
+
+pattern1_social_pyramid = [
+    {"LOWER": {"IN": ["ask", "invite", "refer", "add", "sign"]}},
+    {"IS_ALPHA": True, "OP": "*"},
+    {"LOWER": {"IN": ["friends", "contacts", "neighbors"]}},
+    {"IS_ALPHA": True, "OP": "*"},
+    {"LOWER": {"IN": ["unlock", "earn", "get", "collect", "gain"]}},
+    {"IS_ALPHA": True, "OP": "*"},
+    {"LOWER": {"IN": ["credits", "rewards", "points", "tokens"]}, "OP": "*"}
+]
+
+pattern2_social_pyramid = [
+    {"LOWER": {"IN": ["unlock", "earn", "get", "collect", "gain"]}},
+    {"IS_ALPHA": True, "OP": "*"},
+    {"LOWER": {"IN": ["credits", "rewards", "points", "tokens"]}}
+]
+
+patterns_social_pyramid = [pattern1_social_pyramid, pattern2_social_pyramid]
+
+# privacy zuckering
+# ==============================================================================
+# upload/sync/share/invite/add....friends/contacts/location....'
+
+pattern1_privacy_zuckering = [
+    {"LOWER": {"IN": ["upload", "sync", "share", "add", "invite"]}, "OP": "?"},
+    {"LOWER": {"IN": ["friends", "contacts", "location"]}, "OP": "?"}
+]
+
+patterns_privacy_zuckering = [pattern1_privacy_zuckering]
+
+# intermediate currency
+# ==============================================================================
+# Buy/get/collect x credits/Badge/reward/token/points/stars'
+
+pattern1_intermediate_currency = [
+    {"LOWER": {"IN": ["buy", "get", "collect", "earn", "gain", "unlock"]}},
+    {"IS_ALPHA": True, "OP": "*"},
+    {"LEMMA": {"IN": ["credit", "reward", "point", "token", "badge", "star"]}}
+]
+
+patterns_intermediate_currency = [pattern1_intermediate_currency]
+
+# price comparison prevention
+# ==============================================================================
+# 'per kg', '/kg', '/ea', 'for each'
+
+pattern1_price_comparison_prevention = [
+    {},
+    {"LOWER": {"IN": ["unit", "kg", "each", "ea", "lb", "pound", "kilogram"]}}
+]
+
+patterns_price_comparison_prevention = [pattern1_price_comparison_prevention]
