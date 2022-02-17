98958232294d23ba05b54001563a8e3908e6c6b5
hasanmansur <smhasanmansur@gmail.com>
2022-02-17 13:12:50 -0500

    added #initial histogram analysis

--- visual_analysis/histogram_analysis/histogram_analysis.py
+++ visual_analysis/histogram_analysis/histogram_analysis.py
@@ -1,25 +1,32 @@
-from matplotlib import pyplot as plt
+
 import cv2
 import glob
+
+import utils
+
+def analyze_histogram(file):
+    #segments go to get_histogram function
+    hist = get_histogram(file)
+    get_contrast(hist)
 
 def get_histogram(image):
     img = cv2.imread(image)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # compute a grayscale histogram
-    hist = cv2.calcHist([img], [0], None, [256], [0, 256])
-    # print(len(hist))
-
+    hist = cv2.calcHist([img], [0], None, [256], [0, 4])
     # plot the histogram
-    plt.figure()
-    plt.title("Grayscale Histogram")
-    plt.xlabel("Bins")
-    plt.ylabel("# of Pixels")
-    plt.plot(hist)
-    plt.xlim([0, 256])
-    plt.show()
+    utils.plot_histogram(hist)
+    return hist
 
-files = [file for file in glob.glob("sample_dp_images/*")]
+def get_contrast(hist):
+    # return json with bbox & classification
+    print(hist.sum())
+    print(hist[0], hist[64], hist[128], hist[192])
+    pass
+
+
+files = [file for file in glob.glob("/home/hasan/Downloads/sample_dp_images/*")]
 files.sort()
 for file in files:
     print('----------- processing: ', file, '-------------')
-    get_histogram(file)
+    analyze_histogram(file) # later we will input bbox
