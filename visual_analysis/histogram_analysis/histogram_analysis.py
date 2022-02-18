6b555c503e562cc74c210fc7e614dc04a0182784
hasanmansur <smhasanmansur@gmail.com>
2022-02-18 11:54:26 -0500

    modified #histogram analysis

--- visual_analysis/histogram_analysis/histogram_analysis.py
+++ visual_analysis/histogram_analysis/histogram_analysis.py
@@ -5,27 +5,41 @@ import glob
 import utils
 
 def analyze_histogram(file):
-    #segments go to get_histogram function
-    hist = get_histogram(file)
-    get_contrast(hist)
+    text_segments_info = []
+    f = open(file)
 
-def get_histogram(image):
+    data = json.load(f)
+    for text_segment in data["texts"]:
+        # image creation from bbox info
+        # to do
+
+        # histogram analysis
+        hist = calculate_histogram(file)
+        hist_info = get_contrast(hist)
+
+        # populate json
+        segment_info = {}
+        segment_info["bbox_info"}] = text_segment
+        segment_info["hist_info"}] = hist_info
+        text_segments_info.append(segment_info)
+
+
+def calculate_histogram(image):
     img = cv2.imread(image)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # compute a grayscale histogram
-    hist = cv2.calcHist([img], [0], None, [256], [0, 4])
+    hist = cv2.calcHist([img], [0], None, [256], [0, 3])
     # plot the histogram
     utils.plot_histogram(hist)
     return hist
 
 def get_contrast(hist):
-    # return json with bbox & classification
-    print(hist.sum())
-    print(hist[0], hist[64], hist[128], hist[192])
-    pass
+    hist /= hist.sum()
+    # print(hist[0], hist[85], hist[170])
+    return {"bin_info": [hist[0], hist[85], hist[170]], "intensity": None}
 
 
-files = [file for file in glob.glob("/home/hasan/Downloads/sample_dp_images/*")]
+files = [file for file in glob.glob("/home/hasan/Downloads/sample_dp_images/data/output/ocr/*.json")]
 files.sort()
 for file in files:
     print('----------- processing: ', file, '-------------')
