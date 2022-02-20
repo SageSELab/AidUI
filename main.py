6838c71458a1f05524f376120fc928b4e25a01d5
hasanmansur <smhasanmansur@gmail.com>
2022-02-20 12:07:16 -0500

    added #spatial analysis module initiated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import glob
 import text_analysis.pattern_matching.matching as pattern_matching
 import visual_analysis.histogram_analysis.histogram_analysis as histogram_analysis
+import spatial_analysis.proximity_analysis.proximity_analysis as proximity_analysis
 import utils.utils as utils
 
 # get input image files
@@ -15,13 +16,17 @@ print(len(ocr_files))
 # iterate over the OCR files
 for i in range(len(ocr_files)):
     print('###################### processing: ', ocr_files[i], '######################')
+
     print("------------text_analysis_output-----------")
     dictionary_text_analysis = pattern_matching.match_patterns(ocr_files[i])
     utils.print_dictionary(dictionary_text_analysis)
+
     print("------------visual_analysis_output-----------")
     image_file = img_files[i]
     dictionary_visual_analysis = histogram_analysis.analyze_histogram(dictionary_text_analysis, image_file)
     utils.print_dictionary(dictionary_visual_analysis)
-    # utils.print_dictionary(dictionary_visual_analysis)
+
     print("------------proximity_analysis_output-----------")
+
+
     print("------------size_analysis_output-----------")
