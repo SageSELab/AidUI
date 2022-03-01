617d1749d09c92cef484b74a0d0de369680a8a57
hasanmansur <smhasanmansur@gmail.com>
2022-03-01 01:29:27 -0500

    updated #synthetic data generation: foreground img dimension processing, anchor position in background img

--- object_detection/synthetic_data_generation/synthetic_data_generation.py
+++ object_detection/synthetic_data_generation/synthetic_data_generation.py
@@ -3,13 +3,29 @@ import glob
 import os
 
 def overlay_object_on_ui(background_img, foreground_img):
+    # read images
     img1 = Image.open(background_img)
-    img2 = Image.open(foreground_img)
-    img1.paste(img2, (50,50), mask = img2)
+    img2 = Image.open(foreground_img).convert("RGBA")
     # img1.show()
+
+    # filename processing
     basename_without_ext_background_img = os.path.splitext(os.path.basename(background_img))[0]
     basename_without_ext_foreground_img = os.path.splitext(os.path.basename(foreground_img))[0]
     filename = basename_without_ext_background_img + "_" + basename_without_ext_foreground_img
+
+    # foreground img dimension processing
+    foreground_prefix = basename_without_ext_foreground_img.split("_")[0]
+    if foreground_prefix in ["like", "star"]:
+        size = (64,64)
+        img2 = img2.resize(size, Image.ANTIALIAS)
+
+    # anchor position in the background img
+    x = img1.size[0] // 2
+    y = img1.size[1] // 2
+    # print(x,y)
+
+    # overlay foreground img on the background img
+    img1.paste(img2, (x,y), img2)
     img1.save("output/" + filename + ".png","PNG")
 
 def generate_synthetic_dataset():
@@ -26,3 +42,12 @@ def generate_synthetic_dataset():
 
 if __name__ == "__main__":
     generate_synthetic_dataset()
+
+
+# to do
+# ----------------------------
+# script update: foreground image resize, random anchor posiiton calculation, check fitting into background img
+# list of objects (priority)
+# collect objects(variations)
+# collect RICO UIs
+# collect web UIs
