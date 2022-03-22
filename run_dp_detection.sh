99a9f8b1ec212c64e3f7c3b15dd2d902c1c9cdb2
hasanmansur <smhasanmansur@gmail.com>
2022-03-22 13:09:04 -0400

    updated #bash script updated

--- run_dp_detection.sh
+++ run_dp_detection.sh
@@ -11,18 +11,18 @@
 # -------------------sample commands-----------------------
 
 # copy input files to object detection module
-cp ~/Downloads/input_ui/* ~/Downloads/dark_pattern_project/DLDarkPatterns/object_detection/object_detection_frcnn_mscoco_boilerplate/inference_data/test
+cp ./input_ui/* ./object_detection/object_detection_frcnn_mscoco_boilerplate/inference_data/test
 echo "--------------------------------------input UIs copied to object detection module----------------------------------------------------------------"
 
 # copy input files to UIED module
-cp ./input_ui/* ./dark_pattern_project/DLDarkPatterns/UIED/data/input
+cp ./input_ui/* ./UIED/data/input
 echo "--------------------------------------input UIs copied to UIED module----------------------------------------------------------------------------"
 
 # activate UIED env (dp_uied3) & execute text content area extraction
 eval "$(conda shell.bash hook)"
 conda activate dp_uied3
 echo "--------------------------------------conda environment dp_uied3 activated-----------------------------------------------------------------------"
-cd ~/Downloads/dark_pattern_project/DLDarkPatterns/UIED/
+cd ./UIED/
 echo "--------------------------------------executing text content area extraction---------------------------------------------------------------------"
 python run_uied.py
 echo "waiting 10s"
@@ -31,7 +31,8 @@ sleep 10s
 # activate object detection env (dl_dp_obj_det_env) & execute object detection inference
 conda activate dl_dp_obj_det_env
 echo "--------------------------------------conda environment dl_dp_obj_det_env activated--------------------------------------------------------------"
-cd ~/Downloads/dark_pattern_project/DLDarkPatterns/object_detection/object_detection_frcnn_mscoco_boilerplate
+cd ..
+cd ./object_detection/object_detection_frcnn_mscoco_boilerplate
 echo "--------------------------------------executing object detection inference-----------------------------------------------------------------------"
 jupyter nbconvert --to notebook --execute object_detection_frcnn_mscoco_inference.ipynb
 echo "waiting 10s"
@@ -39,5 +40,5 @@ sleep 10s
 
 # execute DP detection
 echo "--------------------------------------executing DP detection-------------------------------------------------------------------------------------"
-cd ~/Downloads/dark_pattern_project/DLDarkPatterns/
+cd ../..
 python main.py
